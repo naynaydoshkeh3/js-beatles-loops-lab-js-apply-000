e619ea3a69e3364fa91abafb82a33726501a033c
nadine ebrani <nadinedoshkeh3@gmail.com>
2018-06-05 18:27:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+const musicians = ["John", "Paul", "George", "Ringo"];
+
+const instruments = ["guitar", "bass guitar", "bass", "drums"];
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < 4; i++) {
+      array.push(musicians[i]+ " plays "+ instruments[i]);
+    }
+  return array;
+}
