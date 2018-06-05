e3bd5c9b99b85d9101f045aab297a3d76e03b8e6
nadine ebrani <nadinedoshkeh3@gmail.com>
2018-06-05 18:06:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+function theBeatlesPlay(musicians, instruments){
+  musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  instruments=["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+  for (var i=0; i<4; i++){
+    return musicians[i] + " plays " + instruments[i] + "!";
+    //var obj=[musicians[i]+" plays "+ instruments[i] + "!"];
+    //return obj;
+    }
+  }
+  
+  
+  
+function johnLennonFacts(){
+ const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+    ];
+    facts[i] = 0;
+    while (facts[i] < 4){
+      facts[i] ++;
+      return facts[i]+"!!!";
+    }
+}
+function iLoveTheBeatles(i){
+  do {
+    test= "I love the Beatles!";
+    i++;
+  }
+  while(i<15);
+}
